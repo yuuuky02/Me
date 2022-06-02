01f80d6461acf0831d1ad3117222f8a16230374a
yuuuky02 <kim08146@naver.com>
2022-06-02 15:38:29 +0900

    감정 색상 변경

--- app/src/main/java/com/example/meee/MainActivity2.java
+++ app/src/main/java/com/example/meee/MainActivity2.java
@@ -1,8 +1,11 @@
 package com.example.meee;
 
+import static com.example.meee.R.drawable.neutral;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -15,6 +18,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +27,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -29,7 +35,9 @@ public class MainActivity2 extends AppCompatActivity {
     private static final int REQ_CODE_SELECT_IMAGE = 200;
 
     Bitmap bitmap, imageBitmap;
-    Button btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12;
+    Button btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10;
+    RadioGroup rg;
+    RadioButton rb, rb2, rb3;
     EditText et1;
     TextView tv25;
     ImageView iv2, iv3, iv4, iv5;
@@ -57,8 +65,10 @@ public class MainActivity2 extends AppCompatActivity {
         btn8 = findViewById(R.id.button8);
         btn9 = findViewById(R.id.button9);
         btn10 = findViewById(R.id.button10);
-        btn11 = findViewById(R.id.button11);
-        btn12 = findViewById(R.id.button12);
+        rg = findViewById(R.id.radioGroup);
+        rb = findViewById(R.id.radioButton);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
         et1 = findViewById(R.id.editText1);
         tv25 = findViewById(R.id.textView25);
         iv2 = findViewById(R.id.imageView2);
@@ -66,13 +76,29 @@ public class MainActivity2 extends AppCompatActivity {
         iv4 = findViewById(R.id.imageView4);
         iv5 = findViewById(R.id.imageView5);
 
+        // close
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 저장하기
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         // 앨범 아이콘 선택 시 대화창
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 albumdialog = View.inflate(MainActivity2.this, R.layout.albumdialog, null);
                 iv3 = albumdialog.findViewById(R.id.imageView3);
-                btn10 = albumdialog.findViewById(R.id.button10);
+                btn7 = albumdialog.findViewById(R.id.button7);
                 new AlertDialog.Builder(MainActivity2.this)
                         .setTitle("사진 선택")
                         .setIcon(R.drawable.photo)
@@ -92,7 +118,8 @@ public class MainActivity2 extends AppCompatActivity {
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent);
             }
         });
 
@@ -112,10 +139,35 @@ public class MainActivity2 extends AppCompatActivity {
                         .setPositiveButton("닫기", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                Toast.makeText(getApplicationContext(),"사진 보기를 했습니다", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "사진 보기를 했습니다", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .show();
+            }
+        });
+
+        // "감정" 버튼 클릭 시 색상 변경
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButton: // 좋음
+                        rb.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#002EFF")));
+                        rb2.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        rb3.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        break;
+                    case R.id.radioButton2: // 중간
+                        rb.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        rb2.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#002EFF")));
+                        rb3.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        break;
+                    case R.id.radioButton3: // 나쁨
+                        rb.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        rb2.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
+                        rb3.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#002EFF")));
+                        break;
+                }
+                return;
             }
         });
     }
@@ -143,12 +195,12 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK){
-            if(requestCode == REQ_CODE_SELECT_CAMERA){
+            if(requestCode == REQ_CODE_SELECT_CAMERA){ // 카메라 선택
                 Bundle extras = data.getExtras();
                 imageBitmap = (Bitmap) extras.get("data");
                 iv2.setImageBitmap(imageBitmap);
             }
-            else if (requestCode == REQ_CODE_SELECT_IMAGE){
+            else if (requestCode == REQ_CODE_SELECT_IMAGE){ // 앨범 선택
                 try{
                     Bitmap bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
                     Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap1, iv3.getWidth(), iv3.getHeight(), false);
@@ -158,7 +210,7 @@ public class MainActivity2 extends AppCompatActivity {
                     canvas = new Canvas(bitmap);
                     iv3.setImageBitmap(bitmap);
                     iv2.setImageBitmap(bitmap);
-                    iv3.setOnTouchListener(new View.OnTouchListener() {
+                    iv3.setOnTouchListener(new View.OnTouchListener() { // iv3 에 그림그리기
                         @Override
                         public boolean onTouch(View view, MotionEvent motionEvent) {
                             float x = motionEvent.getX();
@@ -205,16 +257,16 @@ public class MainActivity2 extends AppCompatActivity {
         path = new Path();
     }
 
-    // 색상 변경 : 파랑, 굵기 변경 : 가늘게, 원래대로
+    // Paint 변경
     public void onChangePaint(View v) {
         switch (v.getId()) {
-            case R.id.button11: // 파랑
+            case R.id.button8: // 파랑
                 color = Color.BLUE;
                 break;
-            case R.id.button12: // 가늘게
+            case R.id.button9: // 가늘게
                 width -= 5F;
                 break;
-            case R.id.button13: // 원래대로
+            case R.id.button10: // 원래대로
                 color = Color.RED;
                 width = 15F;
                 break;
